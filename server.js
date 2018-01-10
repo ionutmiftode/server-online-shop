60b54aae449b3729f0ba38855bb1657b444ddd56
Mifi <mifionut@yahoo.com>
2018-01-10 20:02:18 +0200

    add server

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+var splittedClientPath = __dirname.split('\\');
+splittedClientPath.pop();
+const clientPath = splittedClientPath.join('\\');
+
+app.get('/', (request, response) => {
+  	response.sendFile(clientPath + '/index.html');
+});
+
+app.get('/products', (request, response) => {
+  	response.send([{
+  		id: 1
+  	}, {
+  		id: 2
+  	}]);
+});
+
+app.get('/error', (request, response) => {
+  	response.status(500).send('Error');
+});
+
+app.listen(port, (err) => {
+  	if (err) {
+    	return console.log('something bad happened', err)
+  	}
+
+  	console.log(`server is listening on http://localhost:${port}`)
+});
